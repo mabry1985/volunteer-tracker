8e2e39cc702493aaf762a25fbe64d8d72aed903d
mabry1985 <mabry1985@gmail.com>
2019-08-02 11:56:54 -0700

    create Project class

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,59 @@
 class Project
+attr_accessor :title
+attr_reader :id
 
+def initialize(attributes)
+  @title = attributes.fetch(:title)
+  @id = attributes.fetch(:id)
+end
 
+def self.all
+  returned_projects = DB.exec("SELECT * FROM projects;")
+  projects = []
+  returned_projects.each() do |project|
+    title = project.fetch("title")
+    id = project.fetch("id").to_i
+    projects.push(project.new({:title => title, :id => id}))
+  end
+  projects
+end
 
+def save
+  result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+  @id = result.first().fetch("id").to_i
+end
 
+def ==(project_to_compare)
+  self.title() == project_to_compare.title()
+end
 
+def self.clear
+  DB.exec("DELETE FROM projects *;")
+end
 
+def self.find(id)
+  project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+  if project
+    title = project.fetch("title")
+    id = project.fetch("id").to_i
+    project.new({:title => title, :id => id})
+  else
+    nil
+  end
+end
 
+def update(attributes)
+  @title = attributes.fetch(:title)
+  DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+end
+
+def delete
+  DB.exec("DELETE FROM projects WHERE id = #{@id};")
+  DB.exec("DELETE FROM volunteers WHERE project_id = #{@id};")
+end
+
+def volunteers
+  Volunteer.find_by_project(self.id)
+end
 
 end
